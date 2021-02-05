d82f969701195316d7411d9c706aeb061dab84e1
Rob Evans <rne@op71.wildwood>
2021-02-04 19:17:33 -0500

    Use the Adafruit_IO API

--- iot_temp/io_temp.py
+++ iot_temp/io_temp.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 """"Read the current temperature from a w1 device and feed the value to
-io.adafruit (AIO) via MQTT using TLS.
+io.adafruit (AIO).
 
 Assumptions:
     *  Environment variables IO_USERNAME, IO_KEY contain AIO credentials.
@@ -11,9 +11,9 @@ Assumptions:
 
 import glob, os, socket, sys, time
 try:
-    import paho.mqtt.client as mqtt
+    from Adafruit_IO import Client
 except ImportError:
-    sys.exit( "MQTT support is missing; please install: python3-paho-mqtt")
+    sys.exit( "AIO support is missing; please do: pip3 install adafruit-io")
 
 
 def read_temp_raw():
@@ -48,51 +48,30 @@ def now():
     "Return the current date/time as a string"
     return time.strftime( "%a %F %T", time.localtime(time.time()))
 
-def on_connect(client, userdata, flags, rc, properties=None):
-    "Callback to log connect"
-    print(now(), "Connected With Result Code: {}".format(rc))
-
-def on_disconnect(client, userdata, rc):
-    "Callback to log and handle disconnect"
-    client.loop_stop()
-    sys.exit( "{} Client Got Disconnected, rc:{}".format( now, rc))
-
 
 # parameters to control how frequently we report a temperature
 last_temp = -1000
 min_temp_change = 0.2   # only report if changed this much from last_temp
 delay = 15              # seconds to wait before checking temperature again
 
-# MQTT parameters
-broker_host = "io.adafruit.com"
-broker_port = 1883 # 8883 for ssl
+# parameters
 usernm = os.environ["IO_USERNAME"]
 passwd = os.environ["IO_KEY"]
 my_feed = "{}-temp".format( socket.gethostname()) 
-topic = "{}/f/{}".format( usernm, my_feed)
 
-# instantiate client, make settings needed, finally connect to broker
-client = mqtt.Client( client_id=my_feed)
-client.on_connect = on_connect
-client.on_disconnect = on_disconnect
-client.username_pw_set( usernm, passwd)
-# Not for AIO ...
-# client.will_set( topic, "Offline", qos=1)
-#
-# a few more settings needed for TLS
-broker_port = 8883 # 8883 for ssl
-client.tls_set_context()
-#
-client.connect(broker_host, broker_port)
-client.loop_start() # run MQTT I/O in another thread
+# instantiate client
+aio = Client( usernm, passwd)
 
 while True:
     temp_c, temp_f = read_temp()
     # report temp only if it has changed since the last reading ...
     if abs( temp_f - last_temp) >= min_temp_change:
-        last_temp = temp_f
         temp_str = "{:.1f}".format( temp_f)
         # print( now(), temp_str + "°F")
-        client.publish( topic, temp_str)
+        try:
+            aio.send( my_feed, temp_str)    # net problems can cause fail
+            last_temp = temp_f
+        except:
+            pass
     time.sleep( delay) # limits update rate
 
